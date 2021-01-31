5e300ee323009fd3586d099af04205aa9020a643
Anton Kravchenko <anton.kravchenko@dish.com>
2021-01-31 19:02:33 +0200

    Bump version

---
+++ cypress/log.ts
@@ -0,0 +1,36 @@
+/*
+
++ 'cypress-xpath'
+
+ +'chaining'
+    + 'finds h1 within main'
+    + 'finds body outside of main when succumbing to // trap'
+    + 'throws when subject is more than a single element'
+
+  + 'within()'
+    + 'finds body outside of within-subject when succumbing to // trap'
+
+  + 'logging'
+    + 'should log by default'
+    + 'logs the selector when not found'
+    --- 'should not log when provided log: false
+
+  + 'elements'
+    + 'finds h1'
+    + 'returns jQuery wrapped elements'
+    + 'returns primitives as is'
+    + 'gets h1 text'
+    + 'retries until element is inserted'
+
+  + 'primitives'
+    + 'counts h1 elements'
+    + 'returns h1 text content'
+    + 'returns boolean'
+
+  + 'typing'
+    + 'works on text input'
+
+  + 'clicking'
+    + 'on button'
+*/
+export default 1;

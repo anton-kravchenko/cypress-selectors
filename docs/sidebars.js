a1f466205956e2e7ae106a61feae09557dda47cc
Anton Kravchenko <anton.kravchenko@dish.com>
2021-05-10 19:09:20 +0300

    drop sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -5,38 +5,38 @@ module.exports = {
       label: 'Intro',
       id: 'intro',
     },
-    {
-      type: 'category',
-      label: 'Getting Started',
-      items: ['getting-started/basic-usage', 'getting-started/installation'],
-      collapsed: false,
-    },
-    {
-      type: 'category',
-      label: 'API reference',
-      items: [
-        'api-reference/selectors',
-        'api-reference/types',
-        'api-reference/selector-configuration',
-        'api-reference/global-configuration',
-        'api-reference/by',
-      ],
-      collapsed: false,
-    },
-    {
-      type: 'doc',
-      label: 'Recipes',
-      id: 'recipes',
-    },
-    {
-      type: 'doc',
-      label: 'Motivation',
-      id: 'motivation',
-    },
-    {
-      type: 'doc',
-      label: 'Caveats',
-      id: 'caveats',
-    },
+    // {
+    //   type: 'category',
+    //   label: 'Getting Started',
+    //   items: ['getting-started/basic-usage', 'getting-started/installation'],
+    //   collapsed: false,
+    // },
+    // {
+    //   type: 'category',
+    //   label: 'API reference',
+    //   items: [
+    //     'api-reference/selectors',
+    //     'api-reference/types',
+    //     'api-reference/selector-configuration',
+    //     'api-reference/global-configuration',
+    //     'api-reference/by',
+    //   ],
+    //   collapsed: false,
+    // },
+    // {
+    //   type: 'doc',
+    //   label: 'Recipes',
+    //   id: 'recipes',
+    // },
+    // {
+    //   type: 'doc',
+    //   label: 'Motivation',
+    //   id: 'motivation',
+    // },
+    // {
+    //   type: 'doc',
+    //   label: 'Caveats',
+    //   id: 'caveats',
+    // },
   ],
 };
